c0c35067265191c5fd3c8495422ccc289c876b3e
Toan-Dang <65716782+Toan-Dang@users.noreply.github.com>
2021-10-30 16:37:40 +0700

    Revert "FrontEnd"

---
+++ Models/Voucher_detail.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WEB2.Models {
+
+    public class Voucher_detail {
+
+        [Key]
+        public int CustomerID { get; set; }
+
+        public int VoucherID { get; set; }
+        public int Amount { get; set; }
+
+        [ForeignKey("CustomerID")]
+        public Customer Customer { get; set; }
+
+        [ForeignKey("ID")]
+        public Voucher Voucher { get; set; }
+    }
+}
