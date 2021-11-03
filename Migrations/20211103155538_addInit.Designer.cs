21d3c04d1db103e17af7a1d74ce7c4395b8d732b
Toan Dang <19522357@gm.uit.edu.vn>
2021-11-03 22:57:20 +0700

    add init

--- Migrations/20211103155538_addInit.Designer.cs
+++ Migrations/20211103155538_addInit.Designer.cs
@@ -10,7 +10,7 @@ using WEB2.Data;
 namespace WEB2.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20211102143820_addInit")]
+    [Migration("20211103155538_addInit")]
     partial class addInit
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
