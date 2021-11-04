e0b220099b53399f4e761bf0b3b5bc02a155ef95
ThanhDinh <thanhdinh1108@gmail.com>
2021-11-04 23:31:57 +0700

    Add change in migration

--- Migrations/20211104162708_addInit.Designer.cs
+++ Migrations/20211104162708_addInit.Designer.cs
@@ -10,7 +10,7 @@ using WEB2.Data;
 namespace WEB2.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20211104161526_addInit")]
+    [Migration("20211104162708_addInit")]
     partial class addInit
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
