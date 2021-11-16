78f508c745f1fe6cdce5bd45259ec95b873e7070
Toan Dang <19522357@gm.uit.edu.vn>
2021-11-16 15:48:26 +0700

    thanh toán lý hóa

---
+++ Areas/Order/MyConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB2.Areas.Order {
+
+    public class MyConfig {
+        public string Url { get; set; }
+        public string TmnCode { get; set; }
+        public string HashSecret { get; set; }
+        public string Returnurl { get; set; }
+    }
+}
